cbd6fe527127f66105d2573b26cbf296206773bb
ahsansagheer071-a11y <ahsansagheer071@gmail.com>
2026-07-11 15:03:07 +0500

    fix: complete faithful website generation packaging

--- alembic/versions/a1b2c3d4e5f6_add_provider_used_and_widen_business_name.py
+++ alembic/versions/a1b2c3d4e5f6_add_provider_used_and_widen_business_name.py
@@ -8,7 +8,7 @@ from alembic import op
 import sqlalchemy as sa
 
 revision = "a1b2c3d4e5f6"
-down_revision = "f8a1b2c3d4e5"
+down_revision = "1ca584b0146f"
 branch_labels = None
 depends_on = None
 
